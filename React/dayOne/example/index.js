7b04ff0fb8df99a66089f0b2d5b757b3e6f24553
Greg Marsh <gregorytm@users.noreply.github.com>
2022-11-07 13:55:38 -0800

    change file tree

---
+++ React/dayOne/example/index.js
@@ -0,0 +1,12 @@
+class FirstComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Beachcoders</h1>
+        <h1>Beachcoders</h1>
+        <h1>Beaccoders</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<FirstComponent />, document.getElementById("root"));
