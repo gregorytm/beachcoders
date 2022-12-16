47a8b6059f8cf17b828085ebdf757b3f4c0da029
Greg Marsh <gregorytm@users.noreply.github.com>
2022-12-16 14:28:47 -0800

    router example

---
+++ React/dayEight/example-2/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import Dog from "./Dog";
+import About from "./About";
+import Contact from "./Contact";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/contact" style={{ padding: 5 }}>
+          About
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Dog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
