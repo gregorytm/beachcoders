d23d457b8c8c5297cdbf057e95ca50b04c85358a
Greg Marsh <gregorytm@users.noreply.github.com>
2023-03-01 11:37:12 -0800

    pokegame

--- React/dayThree/pokedex/src/Pokegame.js
+++ React/dayThree/pokedex/src/Pokegame.js
@@ -19,7 +19,7 @@ class Pokegame extends Component {
     let hand2 = [...this.props.pokemon];
     while (hand1.length < hand2.length) {
       let randIdx = Math.floor(Math.random() * hand2.length);
-      let randPokemon = hand.splice(randIdx, 1)[0];
+      let randPokemon = hand2.splice(randIdx, 1)[0];
       hand1.push(randPokemon);
     }
     // console.log(hand1);
---
+++ React/pokemon2/src/App.js
@@ -0,0 +1,13 @@
+// import PokeCard from "./PokeCard";
+import PokeDex from "./PokeDex";
+
+function App() {
+  return (
+    <div>
+      {/* <PokeCard id={4} name="Charmander" type="fire" exp={62} /> */}
+      <PokeDex />
+    </div>
+  );
+}
+
+export default App;
